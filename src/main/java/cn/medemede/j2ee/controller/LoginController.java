9a1a3e8af0f9209e8aea14440a79406bc3d45c43
Saber <saber3555@outlook.com>
2017-11-14 11:26:26 +0800

    bate2

--- src/main/java/cn/medemede/j2ee/controller/LoginController.java
+++ src/main/java/cn/medemede/j2ee/controller/LoginController.java
@@ -1,22 +1,76 @@
 package cn.medemede.j2ee.controller;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/signin")
+    private final
+    DefaultKaptcha defaultKaptcha;
+
+    @Autowired
+    public LoginController(DefaultKaptcha defaultKaptcha) {
+        this.defaultKaptcha = defaultKaptcha;
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "sign-in";
+    }
+
+    @PostMapping("/signin")
     public String signIn(){
+        String farword="sign-in";
+
+
+        return farword;
+    }
+    @RequestMapping("/signup")
+    public String signUp(){
         String farword="sign-in";
 
         return farword;
     }
 
-    @RequestMapping("signup")
-    public String signUp(){
-        String farword="sign-in";
+    @RequestMapping("/checkcode")
+    public void getCheckCode(HttpServletResponse response, HttpServletRequest request) throws IOException {
 
-        return farword;
+        byte[] captchaChallengeAsJpeg;
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            //生产验证码字符串并保存到session中
+            String createText = defaultKaptcha.createText();
+            request.getSession().setAttribute("checkcode", createText);
+            //使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream =
+                response.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
     }
 }
--- src/main/java/cn/medemede/j2ee/util/ResultUtil.java
+++ src/main/java/cn/medemede/j2ee/util/ResultUtil.java
@@ -5,8 +5,9 @@ import cn.medemede.j2ee.model.Result;
 
 public class ResultUtil {
 
-    public static Result success(Object o) {
-        Result result = new Result();
+    public static Result<Object> success(Object o) {
+
+        Result<Object> result = new Result<>();
         result.setCode(0);
         result.setMsg("success");
         result.setData(o);
@@ -15,9 +16,11 @@ public class ResultUtil {
     }
 
     public static Result error(Integer code, String msg) {
+
         Result result = new Result();
         result.setCode(code);
         result.setMsg(msg);
         return result;
+
     }
 }
