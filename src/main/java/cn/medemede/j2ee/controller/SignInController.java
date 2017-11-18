5675f7552d9ebbd08a59b8c8d0454d3b750df53b
Saber <saber3555@outlook.com>
2017-11-18 22:12:41 +0800

    signup

--- src/main/java/cn/medemede/j2ee/controller/SignInController.java
+++ src/main/java/cn/medemede/j2ee/controller/SignInController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
@@ -26,7 +27,7 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 
-@Controller
+@RestController
 public class SignInController {
 
     private final
